35c333488f83baa5315faedbc38bd72674159a8d
alunosNET <alunosnet@gmail.com>
2026-02-25 15:06:41 +0000

    Primeiro commit

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,15 +1,26 @@
+using System;
 using UnityEngine;
-
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 2; //velocidade por segundo
     public float salto = 400;
+    public bool IsGrounded = false; //indica se tem os pés no chão
+    public Transform pe_e;
+    public Transform pe_d;
+    public float maxDistance = 0.5f;
     //Referência para rigidbody2d
     Rigidbody2D rb;
+    Animator animator;
+    SpriteRenderer sr;
+    float velocidade;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sr= GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,14 +28,53 @@ public class PlayerMovement : MonoBehaviour
     {
         //Leitura do input do jogador
         float horizontal = Input.GetAxis("Horizontal");
+        //verificar se está a correr
+        if (Input.GetButton("Run"))
+        {
+            horizontal = horizontal * 2;
+        }
+        velocidade=Mathf.Abs(horizontal);
+
+        animator.SetFloat("velocidade", velocidade);
         Vector3 movimento = new Vector3(horizontal, 0, 0);
         //Movimento lateral (esq/dir) do jogador
         transform.position += movimento * Time.deltaTime * speed;
         //Salto
-        //TODO: testar se está no chão
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && IsGrounded)
         {
             rb.AddForce(Vector2.up * salto);
+            //animação saltar
+            animator.SetTrigger("saltar");
+        }
+        //verificar se está grounded
+        VerificaPeChao(pe_d);
+        if (IsGrounded==false)
+            VerificaPeChao(pe_e);
+        //se está com os pés no chão e está a fazer a animação de saltar
+        //parar
+        //Debug.Log(animator.GetCurrentAnimatorStateInfo(0).shortNameHash);
+        if (animator.GetCurrentAnimatorStateInfo(0).shortNameHash==-1481439722 &&
+            IsGrounded)
+        {
+            //TODO: interromper a animação
+            animator.ResetControllerState(true);
+        }
+        //flip do sprite quando está a andar para a esquerda
+        sr.flipX = (horizontal < 0);
+    }
+
+    private void VerificaPeChao(Transform pe)
+    {
+        RaycastHit2D raio = Physics2D.Raycast(pe.position, Vector2.down, maxDistance);
+        //Desenha o raio que deteta o chão
+        Debug.DrawRay(pe.position, Vector3.down * maxDistance, Color.red);
+        if (raio.collider == null)
+        {
+            IsGrounded = false;
+        }
+        else
+        {
+            IsGrounded = true;
         }
     }
 }
---
+++ Assets/ideias.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class ideias : MonoBehaviour
+{
+    /*
+     Ideias:
+        CENAS:
+        Mudar de cena com várias portas
+        Permitir que o player aparece em diferentes posições em função da cena de onde vem
+        Permitir fazer o random da próxima cena
+        Não destruir o inventário quando muda de cena
+        ARMADILHA:
+        Guardar a posição "segura" de dois em dois segundos e quando perder vida volta para a posição segura
+        NPC:
+        Balões de fala
+        Tipo: Amigo ou inimigo
+        Estados: Idle, Patrulha, Atacar, Morto
+        SISTEMA MENSAGENS:
+        Mostrar um balão com um texto durante alguns segundos com uma imagem
+     */
+}
